6310f11e1e3bf71fd4312f5dc60ddfb307f2e258
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-03-19 15:13:43 +0100

    +65

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ var price = 0.21 * distance;
 console.log(price , '€');
 
 if (age <= 17) {
-    var finalPrice = ( (price * 20) /100);
+    var finalPrice = ( price - ( (price * 20) / 100) );
     console.log('il prezzo finale è: ', finalPrice ); 
+} else if (age >= 65){
+    var finalPrice = ( price - ( ( price * 40) / 100 ));
+    console.log( yourName, 'il prezzo finale è: ', finalPrice ); 
 }
