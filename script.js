29150bee4a7a6febe2d342b4fa8628e5985d1ef2
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-03-19 14:57:55 +0100

    <18

---
+++ script.js
@@ -0,0 +1,25 @@
+//Insert name of passenger
+
+var yourName = prompt('Inserisci il tuo nome');
+console.log(yourName);
+
+//Insert age
+
+var age = parseInt(prompt('Quanti anni hai?'));
+console.log(age);
+console.log(typeof (age));
+
+//Insert km
+
+var distance = parseInt(prompt('Quanti kilometri devi percorrere?'));
+console.log(distance,' km');
+console.log(typeof (distance));
+
+//Ticket price
+var price = 0.21 * distance;
+console.log(price , '€');
+
+if (age <= 17) {
+    var finalPrice = ( (price * 20) /100);
+    console.log('il prezzo finale è: ', finalPrice ); 
+}
